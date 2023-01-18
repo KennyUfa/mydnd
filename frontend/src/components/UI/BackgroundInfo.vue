81e64d1ad43b1d33d7aa9a062af56149a4905ab4
Kennyufa444@gmail.com <Boiler555>
2023-01-18 09:19:52 +0500

    п

---
+++ frontend/src/components/UI/BackgroundInfo.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="card-text stat">
+    <h4>{{ backgroundName }}</h4>
+    <strong>Описание</strong>:
+    <p v-if="!show">
+      {{ backgroundValue }}
+    </p>
+    <textarea v-else v-model="backgroundValue"></textarea>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BackgroundInfo",
+  props: ['backgroundName', 'backgroundValue', 'show'],
+  watch: {
+    backgroundValue() {
+      this.onUserChange()
+    }
+  },
+  data(){
+    return{
+      saveBackground:true,
+    }
+  },
+  methods: {
+    onUserChange: function () {
+      this.$emit('update:backgroundValue', this.backgroundValue);
+      this.saveBackground=false;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
