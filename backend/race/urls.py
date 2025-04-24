1e2c6f2b68b1ba7df3b2325d5b4078c96d6407c4
Kenny <kennyufa444@gmail.com>
2025-04-24 16:23:18 +0300

    Скобки поправил

--- backend/race/urls.py
+++ backend/race/urls.py
@@ -6,9 +6,9 @@ app_name = 'race'
 
 
 
-urlpatterns = {
+urlpatterns = [
     path('race-list/', RaceListView.as_view(), name='race-list'),
     path('sub-race-list/<int:pk>/', SubRaceListView.as_view(),
          name='sub-race-list'),
     path('character/<int:pk>/sub-race-change/', SubRaceChangeView.as_view(), name='sub-race-change'),
-}
+]
