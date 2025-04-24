65e6e554d788693e1f20eeaa1a6543f0ba7ea619
kenny <kennyufa444@gmail.com>
2025-04-24 21:16:17 +0500

    заполнение бд

--- backend/champion_class/management/commands/create_class.py
+++ backend/champion_class/management/commands/create_class.py
@@ -2,6 +2,15 @@ from django.core.management import BaseCommand
 from .champ_class_create_bace.bard.base import create_bard_base
 from .champ_class_create_bace.bard.arch_one import arch_one, arch_two
 
+from .champ_class_create_bace.legolas_create.leg_base import create_archer_base
+from .champ_class_create_bace.var.var_base import create_var_base
+from .champ_class_create_bace.varavar.varvar_base import create_varvar
+from .champ_class_create_bace.mage.mage_base import create_mage
+from .champ_class_create_bace.monah.monah_base import create_monah
+from .champ_class_create_bace.priest.priest_base import create_cleric
+from .champ_class_create_bace.rogue.rogue_base import create_rogue
+
+
 
 class Command(BaseCommand):
     help = 'Добавление заклинаний'
@@ -10,5 +19,10 @@ class Command(BaseCommand):
         bard = create_bard_base()
         arch_one(bard)
         arch_two(bard)
-
-
+        archer = create_archer_base()
+        var = create_var_base()
+        varvar = create_varvar()
+        mage = create_mage()
+        monah = create_monah()
+        cleric = create_cleric()
+        rogue = create_rogue()
